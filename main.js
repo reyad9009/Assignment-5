5e3888978f685be54dc6ae6340051f88fddb8e0b
reyad9009 <tarekgithub09@gmail.com>
2024-09-24 01:00:05 +0600

    add js function for hidden & show the history &  donation

---
+++ main.js
@@ -0,0 +1,16 @@
+// if click historyBtn hide Donation Section and  Show the  History section
+function historyBtn() {
+    const historySection = document.getElementById("history");
+        historySection.classList.remove('hidden');  // Show the section
+
+    const donationSection = document.getElementById("donation");
+        donationSection.classList.add('hidden');
+           
+}
+
+// if click DonationBtn hide History Section and Show the  Donation Section
+function donationBtn(){
+    const donationSection = document.getElementById("donation");
+        donationSection.classList.remove("hidden");
+}
+
